15c9885061392afd260767adde2631de4cf21ae0
RankPixel <120324754+InsightAcademy@users.noreply.github.com>
2024-09-20 00:35:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx8GJMa6vBUd4Y8QJfIik4ey5N8gE7vVQfDZNZDFR62c2C6gD1AEabofdQnbp7sHaU/exec'
-const form = document.forms['rank-form']
-form.addeventlistener('Submit', e => {
-    e.preventDefault ()
-fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-.then(response => alert('Thank you!'))
-.then (()=> {window.location.reload(); })
-.catch(error => console.error('error!', error.message))
-})
+window.addEventListener("load", function() {
+  const form = document.getElementById('rank-form');
+  form.addEventListener("sub", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
